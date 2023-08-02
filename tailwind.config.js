a80aa7c77fabf2d9e3ff03a7665e67c3cf3117dc
Hernando <hernandoe8@gmail.com>
2023-08-02 15:59:04 -0300

    project travel 0.0

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html", "./src/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      backgroundImage: {
+        'sanFrancisco': "url(../img/sanFrancisco.jpg)",
+        'bali': "url(../img/bali.jpg)",
+        'chicago': "url(../img/chicago.jpg)",
+        'europe': "url(../img/europe.jpg)",
+        'iceland': "url(../img/iceland.jpg)",
+        'LA': "url(../img/LA.jpg)",
+        'miami': "url(../img/miami.jpg)",
+        'new_york': "url(../img/new_york.jpg)",
+        'norway': "url(../img/norway.jpg)",
+        'sanFranciscoDesktop': "url(../img/sanFranciscoDesktop.jpg)",
+        'seattle': "url(../img/seattle.jpg)",
+        'switzerland': "url(../img/switzerland.jpg)",
+        'sydney': "url(../img/sydney.jpg)",
+        'yosemite': "url(../img/yosemite.jpg)",
+      },
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'terciary': '#61AEC9'
+      }),
+      textColor: {
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'terciary': '#61AEC9'
+      },
+      fontFamily: {
+        Montserrat:['Montserrat', 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    with: ["responsive", "hover", "focus"],
+    extends: {
+    },
+  },
+  plugins: [],
+}
+
